58148cc1b745523b09b6838954da42133d4c4421
Eric Porter <porter.eric.e@gmail.com>
2023-12-08 17:15:49 -0700

    Include production dbURL

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const Home = require('./models/home');
 
 const MongoStore = require('connect-mongo');
 
-const dbUrl = 'mongodb://localhost:27017/edmondaporter'
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/edmondaporter'
 
 mongoose.connect(dbUrl);
 
