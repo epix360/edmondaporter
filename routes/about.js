d9e5e91a3b1600bc2a003ce3b9d05192ef8b7b4f
Eric Porter <porter.eric.e@gmail.com>
2023-12-08 18:11:18 -0700

    Puts that thing back where it came from, or so help me...

--- routes/about.js
+++ routes/about.js
@@ -14,13 +14,13 @@ const BlogPost = require('../models/blogpost');
 //   }))
   
   router.get('/new', isLoggedIn, catchAsync(async (req, res) => {
-    res.render('about/new', { currentPage: 'about' })
+    res.render('about/new');
   }))
 
-  router.get('/', isLoggedIn, catchAsync(async (req, res) => {
+  router.get('/', catchAsync(async (req, res) => {
     const profile = await Profile.findOne({ id: { $eq: req.params._id } });
     const blogPosts = await BlogPost.find({ profile: profile._id }).sort({ date: -1 });
-    res.render('about/show', {currentPage: 'about'})
+    res.render('about/show', { profile, blogPosts, currentPage: 'about' })
   }))
   
   
